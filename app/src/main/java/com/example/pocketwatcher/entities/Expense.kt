67bb51c35c528afec5f19d49ff335af0578fa234
BrandonMok <brandonmok88@yahoo.com>
2020-04-07 15:29:02 -0400

    Added 'Creator' field for Expense Entitiy

--- app/src/main/java/com/example/pocketwatcher/entities/Expense.kt
+++ app/src/main/java/com/example/pocketwatcher/entities/Expense.kt
@@ -7,7 +7,8 @@ data class Expense(
     @ColumnInfo(name = "title") var title: String,
     @ColumnInfo(name = "value") var value: Double,
     @ColumnInfo(name = "tag") var tag: String?,
-    @ColumnInfo(name = "date") var date: String
+    @ColumnInfo(name = "date") var date: String,
+    @ColumnInfo(name = "creator") var creator: String
 ){
     @ColumnInfo(name = "id")
     @PrimaryKey(autoGenerate = true) var id: Long = 0
