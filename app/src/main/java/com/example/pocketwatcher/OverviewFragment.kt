00e99dfb7ce2a620d2065113c9c10a0a601d8770
brandonmok <brandonmok88@yahoo.com>
2020-04-07 21:35:11 -0400

    Added overview layout, will need another activity to house it to also have nav

--- app/src/main/java/com/example/pocketwatcher/OverviewFragment.kt
+++ app/src/main/java/com/example/pocketwatcher/OverviewFragment.kt
@@ -11,12 +11,15 @@ import android.view.ViewGroup
  */
 class OverviewFragment : Fragment() {
 
+    /**
+     * onCreateView
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false)
-    }
+    }//onCreateView
 
-}
+}//fragment
