7aa2230bea38ee8a28c420f1e3b13cad87a5743c
BrandonMok <brandonmok88@yahoo.com>
2020-04-19 15:06:07 -0400

    Moved onclick beginnings to bindview

--- app/src/main/java/com/example/pocketwatcher/ExpenseListAdapter.kt
+++ app/src/main/java/com/example/pocketwatcher/ExpenseListAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pocketwatcher.entities.Expense
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import kotlin.math.exp
@@ -34,6 +35,19 @@ class ExpenseListAdapter (private var expenseList: MutableList<Expense>,
         vh.value!!.text = "$" + expense.value.toString()
         vh.date!!.text = expense.date
         vh.tag!!.text = expense.tag
+
+        //onclick listener for the clicked recyclerview item
+        vh.itemView.setOnClickListener{
+            val expense = expenseList[position]
+
+            //Log.d("EXPENSE", Gson().toJson(expense))
+
+            //EditDeleteExpenseDialogFragment(expenseListViewModel).show(activity!!.supportFragmentManager, "Add")
+
+            Snackbar.make(it, "Click detected on item ${position+1}", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 
     /**
@@ -93,20 +107,21 @@ class ExpenseListAdapter (private var expenseList: MutableList<Expense>,
             tag = view.findViewById(R.id.tagValueTextView)
             date = view.findViewById(R.id.dateTextView)
 
-            itemView.setOnClickListener {v: View ->     // could just use "it" instead of setting "v"
-                // get which item tapped on
-                val position = adapterPosition
-
-                //EditDeleteExpenseDialogFragment(expenseListViewModel).show(activity!!.supportFragmentManager, "Add")
-
-                /**
-                 * TODO
-                 * Show alertDialog to edit + delete
-                 */
-                Snackbar.make(v, "Click detected on item ${position+1}", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .show()
-            }//onClickListener
+//            itemView.setOnClickListener {v: View ->     // could just use "it" instead of setting "v"
+//                // get which item tapped on
+//                val position = adapterPosition
+//                //val expenseList = expenseList[position]
+//
+//                //EditDeleteExpenseDialogFragment(expenseListViewModel).show(activity!!.supportFragmentManager, "Add")
+//
+//                /**
+//                 * TODO
+//                 * Show alertDialog to edit + delete
+//                 */
+//                Snackbar.make(v, "Click detected on item ${position+1}", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null)
+//                    .show()
+//            }//onClickListener
         }//init
     }//viewholder
 }//class
